d169cebb91c57789f0bfc77d464ee36f97d317b7
kage <81172243+kage95@users.noreply.github.com>
2022-06-14 23:43:50 +0900

    fizzbuzz

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -1,11 +1,17 @@
-def fizz_buzz(n)
-  if n % 15 == 0
-    "fizzbuzz"
-  elsif n % 3 == 0
-    "fizz"
-  elsif n % 5 == 0
-    "buzz"
-  else
-    n.to_s
+def fizz_buzz(count)
+  n = 1
+  count.times do
+    if n % 15 == 0
+      puts "fizzbuzz"
+    elsif n % 3 == 0
+      puts "fizz"
+    elsif n % 5 == 0
+      puts "buzz"
+    else
+      puts n.to_s
+    end
+    n += 1
   end
 end
+
+puts fizz_buzz(50)
